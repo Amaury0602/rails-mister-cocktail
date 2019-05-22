e3cfaee0191946b803816c0387f524adedfb7001
amaury ropert <amauryropert@gmail.com>
2019-05-22 16:56:41 +0200

    finalisé

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  validates :name, presence: true, allow_blank: false, uniqueness: true
   mount_uploader :photo, PhotoUploader
+  validates :name, presence: true, allow_blank: false, uniqueness: true
+  validates :photo, presence: true
 end
